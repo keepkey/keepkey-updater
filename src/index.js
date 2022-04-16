4faaf58b2fff05ac9d48069bdf52f0df59eee5be
MrNerdHair <mrnerdhair@gmail.com>
2022-04-16 14:52:50 -0400

    get it building and bump version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './axiom.semantic-ui/semantic.min.css'
+import './semantic.min.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
