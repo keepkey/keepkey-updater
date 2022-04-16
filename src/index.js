c797e90f1765bd2ef7a1fdb9699c32ddcb8e2c3b
MrNerdHair <mrnerdhair@gmail.com>
2022-04-16 19:58:31 -0400

    remove unused serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './semantic.min.css'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
